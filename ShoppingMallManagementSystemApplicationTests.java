4febf652ec04c70dcc095d07d10628a14229bbf8
Santhoshi1512 <98403339+Santhoshi1512@users.noreply.github.com>
2022-02-24 20:03:24 +0530

    Add files via upload

---
+++ ShoppingMallManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package example.ShoppingMallManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
